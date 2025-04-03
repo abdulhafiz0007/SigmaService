b4f7dcf0a4e505dc91be962fb4d5693b55155fd6
abdulhafiz0007 <mrsteves77@gmail.com>
2025-04-03 17:46:35 +0500

    change vite configjs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SigmaService/',
+  plugins: [react()],
+  base: '/SigmaService/', 
 })
